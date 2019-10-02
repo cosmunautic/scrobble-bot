7f342ebade2cdf2f745c7157dbbf2ba82932d3a3
Jas <52805313+cosmunautic@users.noreply.github.com>
2019-10-02 01:31:02 -1000

    Update botload.py

--- botload.py
+++ botload.py
@@ -17,8 +17,7 @@ API_SECRET = "189741a760592902ea7de9c39a53ab68"
 BOT_PREFIX = ("**", "Song #")
 TOKEN = "NTk5MDE5NTM4ODE4MDA3MDU1.XSfGyA.ZmD23e7VijZ7n_so0NNVwfhF7NE"
 client = Bot(command_prefix=BOT_PREFIX)
-username = "cosmunautic"
-password_hash = pylast.md5("Pikachu*37")
+
 username_w = "scrobble_bot"
 password_hash_w = pylast.md5("mypr0ject!")
 
